fcddf259050050f1e03a492f622c80e520c7a11f
tae beok <joo98e@gmail.com>
2021-06-13 19:26:38 +0900

    video card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Typography } from '@material-ui/core';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardUI from './components/CardUI';
+
+// import PropTypes from 'prop-types';
+
+// App.PropTypes = {
+//     id : PropTypes.number
+// }
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      num: 0
+    }
+  }
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+    console.log(this.state);
+  }
+  plus = () => {
+    this.setState(current => ({ num: current.num + 1 }))
+  };
+
+  minus = () => {
+    this.setState(current => ({ num: current.num - 1 }))
+  };
+
+  componentDidMount() {
+    console.log('컴포넌트 마운트(태어남) 완료');
+    this.getMovies();
+  }
+
+  componentDidUpdate() {
+    console.log('컴포넌트 업데이트 완료');
+  }
+
+  componentWillUnmount() {
+    console.log('컴포넌트가 언마운트 되었음');
+  }
+
+  render() {
+    console.log('컴포넌트 렌더링 시작');
+
+    const { num, isLoading, movies } = this.state;
+    return (
+      <div>
+        
+
+        <Typography>num : {num}</Typography>
+        <Button onClick={this.plus} variant="contained" color="primary">
+          PLUS
+        </Button>
+        <Button onClick={this.minus} variant="contained" color="secondary">
+          MINUS
+        </Button>
+
+        {isLoading ? "is Loading" : movies.map(movie => {
+          console.log(movies);
+          return <CardUI
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            url={movie.url}
+            poster={movie.large_cover_image}
+          />
+        })}
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/CardUI.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import {
+    withStyles,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+    Link
+} from '@material-ui/core';
+
+const styles = theme => ({
+    cardStyle: {
+        maxWidth: 300,
+        color: "#FFF",
+        backgroundColor: "#8C8C8C"
+    }
+});
+
+class CardUI extends Component {
+    render() {
+
+        const { classes, id, title, url, poster } = this.props;
+        console.log(classes);
+        return (
+            <Card className={classes.cardStyle}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        alt="poster"
+                        image={poster}
+                        title={poster}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {id}
+                        </Typography>
+                        <Typography gutterBottom color="textPrimary" component="p">
+                            {title}
+                        </Typography>
+                        <Link>{url}</Link>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        )
+    }
+}
+
+
+export default withStyles(styles)(CardUI);
